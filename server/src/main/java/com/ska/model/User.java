ff791878b57ce0b7f6fcef02341005b1d48af802
OneNull <shavrin_ns@mail.ru>
2025-08-09 10:16:56 +0300

    User entity realization

---
+++ server/src/main/java/com/ska/model/User.java
@@ -0,0 +1,67 @@
+package com.ska.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+
+
+    public User() {}
+
+
+    public final void setEmail(final String email) {
+        this.email = email;
+    }
+    public final void setPassword(final String password) {
+        this.password = password;
+    }
+
+    public final Long getId() {
+        return this.id;
+    }
+    public final String getEmail() {
+        return this.email;
+    }
+    public final String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public final boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+
+        if (obj == null || obj.getClass() != this.getClass())
+            return false;
+
+        User user = (User) obj;
+        return java.util.Objects.equals(user.id, this.id);
+    }
+
+    @Override
+    public final int hashCode() {
+        return java.util.Objects.hash(this.id);
+    }
+
+    @Override
+    public final String toString() {
+        return "User{" +
+            "id=" + this.id +
+            ", email=" + this.email +
+            "}";
+    }
+
+}
