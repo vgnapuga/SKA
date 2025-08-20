2b12b03c5c05b0c05bcc94b47889bd2c4f66fb6a
OneNull <shavrin_ns@mail.ru>
2025-08-20 14:01:42 +0300

    EncryptedValueObject refactoring from Base64 String to encryptedBytes

--- server/src/main/java/com/ska/vo/encryptable/EncryptedValueObject.java
+++ server/src/main/java/com/ska/vo/encryptable/EncryptedValueObject.java
@@ -1,33 +1,27 @@
 package com.ska.vo.encryptable;
 
-import java.util.Base64;
-
 import com.ska.exception.DomainValidationException;
 import com.ska.vo.BaseValueObject;
 
 
-public abstract class EncryptedValueObject extends BaseValueObject<String> {
+public abstract class EncryptedValueObject extends BaseValueObject<byte[]> {
 
-    protected EncryptedValueObject(final String encryptedValue) {
-        super(encryptedValue);
+    protected EncryptedValueObject(final byte[] encryptedBytes) {
+        super(encryptedBytes);
     }
 
 
     @Override
-    protected void checkValidation(final String encryptedValue) {
-        validateNotBlank(encryptedValue);
-        validateBase64Format(encryptedValue);
-        validateReasonableSize(encryptedValue);
+    protected void checkValidation(final byte[] encryptedBytes) {
+        validateNotEmpty(encryptedBytes);
+        validateReasonableSize(encryptedBytes);
     }
 
-    private void validateBase64Format(final String encryptedValue) {
-        try {
-            Base64.getDecoder().decode(encryptedValue);
-        } catch (IllegalArgumentException e) {
-            throw new DomainValidationException(this.getClass().getSimpleName() + " has invalid encrypted data format");
-        }
+    private final void validateNotEmpty(final byte[] encryptedBytes) {
+        if (encryptedBytes.length == 0)
+            throw new DomainValidationException(this.getClass().getSimpleName() + " data is empty");
     }
 
-    protected abstract void validateReasonableSize(final String encryptedValue);
+    protected abstract void validateReasonableSize(final byte[] encryptedBytes);
     
 }
